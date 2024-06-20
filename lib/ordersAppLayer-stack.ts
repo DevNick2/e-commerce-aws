cd97dd91752ee6bf205d9c10eabe62b08cc0e224
Jean Nicolas <jeannicolasav@gmail.com>
2024-06-20 11:13:54 -0300

    Feat: Nova layer para o OrderEvent
    1) Criado a nova layer para executar o código da orderEventsLayer, que vai ficar responsavel pelas mensagens do SNS, registrado no SSM (AWS System Manager); 2) Melhorias nos comentarios

--- lib/ordersAppLayer-stack.ts
+++ lib/ordersAppLayer-stack.ts
@@ -8,30 +8,43 @@ export class OrdersAppLayerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const ordersLayer = new lambda.LayerVersion(this, 'OrdersLayer', {
-      code: lambda.Code.fromAsset('lambda/orders/layers/ordersLayer'),
+    const ordersLayer = new lambda.LayerVersion(this, 'OrdersLayer' /* Identificação lá no Cloud Formation */, {
+      code: lambda.Code.fromAsset('lambda/orders/layers/ordersLayer'), // onde o código que vai ser executado esta
       compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
       layerVersionName: 'OrdersLayer',
       removalPolicy: cdk.RemovalPolicy.RETAIN,
     })
 
-    // Identificação lá no Cloud Formation
-    new ssm.StringParameter(this, 'OrdersLayerVersionArn', {
+    // Salva no parameter store (SSM) o ARN (Amazon Resource Name) do ordersLayer
+    new ssm.StringParameter(this, 'OrdersLayerVersionArn' /* Identificação lá no Cloud Formation */, {
       parameterName: 'OrdersLayerVersionArn',
       stringValue: ordersLayer.layerVersionArn
     })
     
     const ordersApiLayer = new lambda.LayerVersion(this, 'OrdersApiLayer', {
-      code: lambda.Code.fromAsset('lambda/orders/layers/ordersApiLayer'),
+      code: lambda.Code.fromAsset('lambda/orders/layers/ordersApiLayer'), // onde o código que vai ser executado esta
       compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
       layerVersionName: 'OrdersApiLayer',
       removalPolicy: cdk.RemovalPolicy.RETAIN,
     })
 
-    // Identificação lá no Cloud Formation
-    new ssm.StringParameter(this, 'OrdersApiLayerVersionArn', {
+    // Salva no parameter store (SSM) o ARN (Amazon Resource Name) do ordersApiLayer
+    new ssm.StringParameter(this, 'OrdersApiLayerVersionArn' /* Identificação lá no Cloud Formation */, {
       parameterName: 'OrdersApiLayerVersionArn',
       stringValue: ordersApiLayer.layerVersionArn
+    })
+
+    const orderEventsLayer = new lambda.LayerVersion(this, 'OrderEventsLayer', {
+      code: lambda.Code.fromAsset('lambda/orders/layers/ordersEventsLayer'), // onde o código que vai ser executado esta
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      layerVersionName: 'OrderEventsLayer',
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    })
+
+    // Salva no parameter store (SSM) o ARN (Amazon Resource Name) do orderEventsLayer
+    new ssm.StringParameter(this, 'OrdersEventsLayerVersionArn' /* Identificação lá no Cloud Formation */, {
+      parameterName: 'OrdersEventsLayerVersionArn',
+      stringValue: orderEventsLayer.layerVersionArn
     })
   }
 }

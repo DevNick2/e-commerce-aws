733a624fd9fb1827d8e7f40de367219d947fffcb
Jean Nicolas <jeannicolasav@gmail.com>
2024-06-20 11:11:42 -0300

    Feat: Preparacao para a funcao do SNS
    1) Nova funcao lambda para os eventos do SNS e inscricao da function no SNS; 2) Adicionado o layer orderEventLayer e a variavel ORDER_EVENTS_TOPIC_ARN na funcao de pedidos; 3) Criado uma nova instancia do SNS chamada ordersTopic; 4) Melhorias dos comentarios para deixar mais claro; 5) Importacao do SNS e do SNS Subscription; 6) Adicionado a props eventsDdb a interface do OrdersAppStackProps

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -3,11 +3,15 @@ import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as cdk from 'aws-cdk-lib'
 import * as db from 'aws-cdk-lib/aws-dynamodb'
 import * as ssm from 'aws-cdk-lib/aws-ssm' // recurso AWS Systems Manager para guardar paramatros na aws
+import * as sns from 'aws-cdk-lib/aws-sns'
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions'
+
 
 import { Construct } from 'constructs'
 
 interface OrdersAppStackProps extends cdk.StackProps {
-  productsDdb: db.Table
+  productsDdb: db.Table,
+  eventsDdb: db.Table
 }
 
 export class OrdersAppStack extends cdk.Stack {
@@ -17,15 +21,22 @@ export class OrdersAppStack extends cdk.Stack {
     super(scope, id, props)
 
     // Importando os layers do parameter store:
-    // OrdersLayerVersionArn e ProductsLayerVersionArn
-    const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, 'OrdersLayerVersionArn')
-    const ordersLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'OrdersLayerVersionArn', ordersLayerArn)
+
+    // Order Layer (Function)
+    const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, 'OrdersLayerVersionArn') // busca o ARN do OrdersLayerVersionArn do parameter store
+    const ordersLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'OrdersLayerVersionArn', ordersLayerArn) // instancia a função que busca pelo ARN do ordersLayerArn pelo lambda.LayerVersion
     
-    const ordersApiLayerArn = ssm.StringParameter.valueForStringParameter(this, 'OrdersApiLayerVersionArn')
-    const ordersApiLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'OrdersApiLayerVersionArn', ordersApiLayerArn)
+    // Order Api Layer (Api Gateway)
+    const ordersApiLayerArn = ssm.StringParameter.valueForStringParameter(this, 'OrdersApiLayerVersionArn') // busca o ARN do OrdersApiLayerVersionArn do parameter store
+    const ordersApiLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'OrdersApiLayerVersionArn', ordersApiLayerArn) // instancia a função que busca pelo ARN do ordersApiLayerArn pelo lambda.LayerVersion
     
-    const productLayerArn = ssm.StringParameter.valueForStringParameter(this, 'ProductsLayerVersionArn')
-    const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'ProductsLayerVersionArn', productLayerArn)
+    // Product Layer (Function)
+    const productLayerArn = ssm.StringParameter.valueForStringParameter(this, 'ProductsLayerVersionArn') // busca o ARN do ProductLayer do parameter store
+    const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'ProductsLayerVersionArn', productLayerArn) // instancia a função que busca pelo ARN do productLayerArn pelo lambda.LayerVersion
+
+    // Order Event Layer (Topico SNS)
+    const orderEventLayerArn = ssm.StringParameter.valueForStringParameter(this, 'OrdersEventsLayerVersionArn') // busca o ARN do OrdersEventsLayerVersionArn do parameter store
+    const orderEventLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'orderEventLayerArn', orderEventLayerArn) // instancia a função que busca pelo ARN do orderEventLayerArn pelo lambda.LayerVersion
 
     const ordersDdb = new db.Table(this, 'OrdersDdb', {
       tableName: 'orders',
@@ -43,7 +54,14 @@ export class OrdersAppStack extends cdk.Stack {
       writeCapacity: 1
     })
 
-    this.ordersHandler = new lambdaNodeJs.NodejsFunction(this, 'OrdersFunction', {
+    // Criando uma instancia do SNS
+    const ordersTopic = new sns.Topic(this, 'OrdersEventTopic', {
+      displayName: 'OrdersEventTopic',
+      topicName: 'order-event'
+    })
+
+    // Função de pedidos
+    this.ordersHandler = new lambdaNodeJs.NodejsFunction(this, 'OrdersFunction', { // função de pedidos
       functionName: 'OrdersFunction',
       entry: 'lambda/orders/ordersFunction.ts',
       handler: 'handler',
@@ -53,17 +71,43 @@ export class OrdersAppStack extends cdk.Stack {
         minify: true,
         sourceMap: false
       },
-      layers: [ordersLayer, productsLayer, ordersApiLayer],
+      layers: [ordersLayer, productsLayer, ordersApiLayer, orderEventLayer], // Adiciona os layers para compartilhar com a função de pedidos
       environment: {
         PRODUCTS_DDB: props.productsDdb.tableName,
-        ORDERS_DDB: ordersDdb.tableName
+        ORDERS_DDB: ordersDdb.tableName,
+        ORDER_EVENTS_TOPIC_ARN: ordersTopic.topicArn
       },
       runtime: lambda.Runtime.NODEJS_20_X,
-      tracing: lambda.Tracing.ACTIVE, // gera impacto no custo da operação
+      tracing: lambda.Tracing.ACTIVE, // gera impacto no custo da operação pois precisa gerar um trace nos logs
       insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
     })
 
+    // Definindo permissões
     ordersDdb.grantReadWriteData(this.ordersHandler)
     props.productsDdb.grantReadData(this.ordersHandler)
+    ordersTopic.grantPublish(this.ordersHandler) // Concedendo permissão para a função de pedidos publicar no SNS
+
+    // Função dos eventos dos pedidos (SNS)
+    const ordersEventsHandler = new lambdaNodeJs.NodejsFunction(this, 'OrdersEventsFunction', { // função de pedidos
+      functionName: 'OrdersEventsFunction',
+      entry: 'lambda/orders/orderEventsFunction.ts',
+      handler: 'handler',
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(2),
+      bundling: {
+        minify: true,
+        sourceMap: false
+      },
+      environment: {
+        EVENTS_DDB: props.eventsDdb.tableName
+      },
+      layers: [orderEventLayer], // Adiciona os layers para compartilhar com a função de pedidos
+      runtime: lambda.Runtime.NODEJS_20_X,
+      tracing: lambda.Tracing.ACTIVE, // gera impacto no custo da operação pois precisa gerar um trace nos logs
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
+    })
+    
+    // inscrevendo a função ordersEventsHandler no SNS
+    ordersTopic.addSubscription(new subs.LambdaSubscription(ordersEventsHandler))
   }
 }
